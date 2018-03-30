f9ee62b963025e7852947163c5dce137243a764c
pasak <pasaksoftware@gmail.com>
2018-03-30 22:20:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -47,7 +47,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
  			$messages = [
 				'type' => 'text',
-				'text' => 'type ' . $jsonObject['type']
+				'text' => $json
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
