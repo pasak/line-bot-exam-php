15e8ebe68b21f97a9dd7fc703f082c3d30c49217
pasak <pasaksoftware@gmail.com>
2018-03-31 16:27:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -78,7 +78,7 @@ $messages = [
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
