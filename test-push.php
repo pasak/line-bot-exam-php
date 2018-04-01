da96f539224903191ecd81029b5e877dd1f6bfe0
pasak <pasaksoftware@gmail.com>
2018-04-01 11:41:19 +0700

    Update test-push.php

--- test-push.php
+++ test-push.php
@@ -2,25 +2,25 @@
 $access_token = '0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
 
 $messages = [
-  ["type":"text","text":"ดูโปรแกรม ดอยอินทนนท์, ขุนวาง, ดอยสุเทพ, ดอยปุย\nวันที่ 1"],
+  ["type"=>"text","text"=>"ดูโปรแกรม ดอยอินทนนท์, ขุนวาง, ดอยสุเทพ, ดอยปุย\nวันที่ 1"],
   [
-    "type":"template",
-    "altText":"สถานที่ท่องเที่ยว วันที่ 1",
-    "template":[
-      "type":"carousel",
-      "columns":[
+    "type"=>"template",
+    "altText"=>"สถานที่ท่องเที่ยว วันที่ 1",
+    "template"=>[
+      "type"=>"carousel",
+      "columns"=>[
         [
-          "thumbnailImageUrl":"https://tripmaster.co/images/attraction/1/s/10.jpg",
-          "title":"ดอยอินทนนท 3 ชม.",
-          "actions":[
-            ["type":"message","label":"ดูรูปเพิ่มเติม","text":"ดูรูปเพิ่มเติม ดอยอินทนนท์"]
+          "thumbnailImageUrl"=>"https://tripmaster.co/images/attraction/1/s/10.jpg",
+          "title"=>"ดอยอินทนนท 3 ชม.",
+          "actions"=>[
+            ["type"=>"message","label"=>"ดูรูปเพิ่มเติม","text"=>"ดูรูปเพิ่มเติม ดอยอินทนนท์"]
           ]
         ],
         [
-          "thumbnailImageUrl":"https://tripmaster.co/images/attraction/11/s/110.jpg",
-          "title":"ขุนวาง 2 ชม.",
-          "actions":[
-            ["type":"message","label":"ดูรูปเพิ่มเติม","text":"ดูรูปเพิ่มเติม ขุนวาง"]
+          "thumbnailImageUrl"=>"https://tripmaster.co/images/attraction/11/s/110.jpg",
+          "title"=>"ขุนวาง 2 ชม.",
+          "actions"=>[
+            ["type"=>"message","label"=>"ดูรูปเพิ่มเติม","text"=>"ดูรูปเพิ่มเติม ขุนวาง"]
           ]
         ]
       ]
