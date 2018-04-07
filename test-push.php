b788a1e34bc8705f168c0a749681ebb95b3b276f
pasak <pasaksoftware@gmail.com>
2018-04-07 10:49:59 +0700

    Update test-push.php

--- test-push.php
+++ test-push.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
 
-$json = '[{"type":"template","altText":"รายชื่อโรงแรม 1","template":{"type":"carousel","columns":[{"thumbnailImageUrl":"https:\/\/tripmaster.co\/upload\/hotel\/logo\/1.jpg","title":"โรงแรมตัวอย่าง","text":"ราคาเริ่มต้น 2,001 บาท","actions":[{"type":"message","label":"ดูห้องพัก","text":"ดูห้องพัก โรงแรมตัวอย่าง เมืองเชียงใหม่"}]}]}}]';
+$json = '[{"type":"template","altText":"รายชื่อโรงแรม 1","template":{"type":"carousel","columns":[{"thumbnailImageUrl":"https:\/\/tripmaster.co\/upload\/hotel\/logo\/1.jpg","title":"โรงแรมตัวอย่าง","text":"ราคาเริ่มต้น 2,001 บาท","actions":[{"type":"message","label":"VIEW","text":"ดูห้องพัก โรงแรมตัวอย่าง เมืองเชียงใหม่"}]}]}}]';
 
 $messages = json_decode($json,true);
 			// Make a POST Request to Messaging API to reply to sender
