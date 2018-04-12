17f8c2a2cb23ef5b12ab05cc761fa641d9991fac
pasak <pasaksoftware@gmail.com>
2018-04-12 12:40:19 +0700

    Update tourist-messages.php

--- tourist-messages.php
+++ tourist-messages.php
@@ -10,7 +10,7 @@ $data = [
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
