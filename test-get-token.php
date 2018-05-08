a56b49d9cecb69ca667b3b08b7f3bc571568afc4
pasak <pasaksoftware@gmail.com>
2018-05-08 16:12:55 +0700

    Create test-get-token.php

---
+++ test-get-token.php
@@ -0,0 +1,22 @@
+<?php
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/user/U89c37c01fe7219f134382e1dc7e896ba/linkToken';
+
+// $data = ['to' => $LineUserID,'messages' => $messages];
+
+// $post = json_encode($data);
+
+$access_token = '0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
+
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result, "\r\n<br><br>";
+?>
