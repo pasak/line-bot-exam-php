5e1d736bf27ceba82a2a891f1771873bd2453404
pasak <pasaksoftware@gmail.com>
2018-05-10 08:57:01 +0700

    Update test-push.php

--- test-push.php
+++ test-push.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = '0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'aXp5EzJtW7Y7wIo2iQ62+uQIxoQw1VwnycYaxc3lsNPHxCpeEYkFDXtDdCgwBLIqiQHYkudjd7W6BNgarFfP7UZlQCfheQNS/ABOhs3KA5TTm91n4IPcnz0TThUdQ9pz/gc7KehYQVPIp938hMUZMQdB04t89/1O/w1cDnyilFU=';
+// 	'0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
 
 $json = '[{"type":"template","altText":"ดูห้องพัก โรงแรมตัวอย่าง เมืองเชียงใหม่","template":{"type":"carousel","columns":[{"thumbnailImageUrl":"https:\/\/tripmaster.co\/upload\/hotel\/room\/1.png","text":"ดีลักซ์ ทวิน","actions":[{"type":"message","label":"ดูราคา","text":"ดูราคา 5 ดีลักซ์ ทวิน"},{"type":"message","label":"ดูรายละเอียด","text":"ดูรายละเอียด 5 ดีลักซ์ ทวิน"}]},{"thumbnailImageUrl":"https:\/\/tripmaster.co\/upload\/hotel\/room\/9.png","text":"ดีลักซ์ ดับเบิล","actions":[{"type":"message","label":"ดูราคา","text":"ดูราคา 6 ดีลักซ์ ดับเบิล"},{"type":"message","label":"ดูรายละเอียด","text":"ดูรายละเอียด 6 ดีลักซ์ ดับเบิล"}]}]}}]';
 //	'[{"type":"sticker","packageId":"2","stickerId":"41"},{"type":"text","text":"สรุปรายการจองห้องพัก\nโรงแรมตัวอย่าง เมืองเชียงใหม่\nดีลักซ์ ดับเบิล\nราคาคนกันเอง!\nวันที่ 20 พฤษภาคม 2561\n1 คืน\n1 ห้อง\nรวม 1,000 บาท\nราคารวมค่าบริการและภาษี"},{"type":"template","altText":"ยืนยันการจอง","template":{"type":"buttons","text":"ยืนยันการจอง","actions":[{"type":"message","label":"Yes","text":"ยืนยันการจอง 6 1 +BF ดีลักซ์ ดับเบิล ราคาคนกันเอง! เพิ่มอาหารเช้า"},{"type":"message","label":"No","text":"ปฏิเสธการจอง"}]}}]';
@@ -8,8 +9,8 @@ $messages = json_decode($json,true);
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'to' => 'Ufe4c105ec12e9334813a95dc5c5e758b',
-// 				'to' => 'U89c37c01fe7219f134382e1dc7e896ba',
+// 				'to' => 'Ufe4c105ec12e9334813a95dc5c5e758b',
+				'to' => 'U89c37c01fe7219f134382e1dc7e896ba',
 				'messages' => $messages,
 			];
 			$post = json_encode($data);
