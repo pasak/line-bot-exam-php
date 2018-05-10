6b994bed7ac816ff1dd6faa4913bafa3938b931c
pasak <pasaksoftware@gmail.com>
2018-05-10 08:37:41 +0700

    Update provider-reply.php

--- provider-reply.php
+++ provider-reply.php
@@ -5,7 +5,7 @@ $access_token = str_replace(' ','+',$_REQUEST['Token']);
 // $access_token = 'TM4mFmMA3jm4rYTSd4ZSl+V4Pj3Jus+qNi4H1RZAIJELfF5JFahiH1Mw+8W5qfEznMozstz4Nw0hE4tPGVfpJwAUrDdxU6XgXBHvdudmWIVTkZLqpSuTjZUmC+oeDsWCJ2d4Rc6LpwC95sS2zFkYuAdB04t89/1O/w1cDnyilFU=';
 
 
-echo '$access_token ', $access_token;
+// echo '$access_token ', $access_token;
 
 $arrContextOptions=array(
       "ssl"=>array(
