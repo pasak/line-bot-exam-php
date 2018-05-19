3cdd021a9d4752c8fb181e2d782caf9283436765
pasak <pasaksoftware@gmail.com>
2018-05-19 17:26:51 +0700

    Update fullfillment.php

--- fullfillment.php
+++ fullfillment.php
@@ -8,7 +8,7 @@ $a = [
   "fulfillmentText" =>
     'คุณต้องการจองโรงแรม ' . $parameters['place'] .' วันที่ '.
     $parameters['date'] .' '. $parameters['room'] .' ห้อง '.
-    $parameters['night'] .' คืน ' ;
+    $parameters['night'] .' คืน '
 ];
 echo json_encode($a);
 ?>
