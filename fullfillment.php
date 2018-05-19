84042c503248b65949a2c993ef466653a8798641
pasak <pasaksoftware@gmail.com>
2018-05-19 18:47:08 +0700

    Update fullfillment.php

--- fullfillment.php
+++ fullfillment.php
@@ -5,6 +5,7 @@ $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 
 $a = [
+  "allRequiredParamsPresent" => true,
   "fulfillmentMessages" => [
       [
         "card" => [
