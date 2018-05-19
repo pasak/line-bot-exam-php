80af3e4ff11c3526efc24c31e990e1ee2c760aa0
pasak <pasaksoftware@gmail.com>
2018-05-19 17:41:57 +0700

    Update fullfillment.php

--- fullfillment.php
+++ fullfillment.php
@@ -6,9 +6,10 @@ $update = json_decode($update_response, true);
 
 $a = [
   "fulfillmentText" =>
-    'คุณต้องการจองโรงแรม ' . $parameters['place'] .' วันที่ '.
-    $parameters['date'] .' '. $parameters['room'] .' ห้อง '.
-    $parameters['night'] .' คืน '
+    'คุณต้องการจองโรงแรม ' . $update['queryResult']['parameters']['place']
+    .' วันที่ ' . $update['queryResult']['parameters']['date'] .' '.
+    $update['queryResult']['parameters']['room'] .' ห้อง '.
+    $update['queryResult']['parameters']['night'] .' คืน '
 ];
 echo json_encode($a);
 ?>
