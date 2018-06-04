d4904aead14f0262ecf2f4304bf30c46c4807475
pasak <pasaksoftware@gmail.com>
2018-06-04 10:03:41 +0700

    Update fullfillment.php

--- fullfillment.php
+++ fullfillment.php
@@ -4,30 +4,129 @@ header('Content-Type: application/json');
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 
-$a = [
-
-  "fulfillmentText" =>
-    'คุณต้องการจองโรงแรม ' . $update['queryResult']['parameters']['place']
-    .' วันที่ ' . $update['queryResult']['parameters']['date'] .' '.
-    $update['queryResult']['parameters']['room'] .' ห้อง '.
-    $update['queryResult']['parameters']['night'] .' คืน '
-];
-/*
-  "fulfillmentMessages" => [
+$a =
+[
+  "speech" => "This is a simple response for a carousel",
+  "data" =>  [
+    "google" =>
+    [
+      "expectUserResponse" => true,
+      "richResponse" =>
       [
-        "card" => [
-          "title" => "โรงแรมตัวอย่าง",
-          "subtitle" => "ราคาเริ่มต้น 1,601 บาท",
-          "imageUri" => "https://tripmaster.co/upload/hotel/logo/1.jpg",
-          "buttons" => [
+        "items" =>
+        [
+          [
+            "simpleResponse" =>
             [
-              "text" => "ดูห้องพัก ",
-              "postback" => "https://dummyhotel.online/"
+              "textToSpeech" => "This is a simple response for a carousel"
+            ]
+          ]
+        ],
+        "suggestions" =>
+        [
+          [
+            "title" => "Basic Card"
+          ],
+          [
+            "title" => "List"
+          ],
+          [
+            "title" => "Carousel"
+          ],
+          [
+            "title" => "Suggestions"
+          ]
+        ]
+      ],
+      "systemIntent" =>
+      [
+        "intent" => "actions.intent.OPTION",
+        "data" =>
+        [
+          "@type" => "type.googleapis.com/google.actions.v2.OptionValueSpec",
+          "carouselSelect" =>
+          [
+            "items" =>
+            [
+              [
+                "optionInfo" =>
+                [
+                  "key" => "title",
+                  "synonyms" =>
+                  [
+                    "synonym of title 1",
+                    "synonym of title 2",
+                    "synonym of title 3"
+                  ]
+                ],
+                "title" => "Title of First List Item",
+                "description" => "This is a description of a carousel item",
+                "image" =>
+                [
+                  "url" => "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                  "accessibilityText" => "Image alternate text"
+                ]
+              ],
+              [
+                "optionInfo" =>
+                [
+                  "key" => "googleHome",
+                  "synonyms" =>
+                  [
+                    "Google Home Assistant",
+                    "Assistant on the Google Home"
+                  ]
+                ],
+                "title" => "Google Home",
+                "description" => "Google Home is a voice-activated speaker powered by the Google Assistant.",
+                "image" =>
+                [
+                  "url" => "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                  "accessibilityText" => "Google Home"
+                ]
+              ],
+              [
+                "optionInfo" =>
+                [
+                  "key" => "googlePixel",
+                  "synonyms" =>
+                  [
+                    "Google Pixel XL",
+                    "Pixel",
+                    "Pixel XL"
+                  ]
+                ],
+                "title" => "Google Pixel",
+                "description" => "Pixel. Phone by Google.",
+                "image" =>
+                [
+                  "url" => "https://storage.googleapis.com/madebygoog/v1/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png",
+                  "accessibilityText" => "Google Pixel"
+                ]
+              ],
+              [
+                "optionInfo" =>
+                [
+                  "key" => "googleAllo",
+                  "synonyms" =>
+                  [
+                    "Allo"
+                  ]
+                ],
+                "title" => "Google Allo",
+                "description" => "Introducing Google Allo, a smart messaging app that helps you say more and do more.",
+                "image" =>
+                [
+                  "url" => "https://allo.google.com/images/allo-logo.png",
+                  "accessibilityText" => "Google Allo Logo"
+                ]
+              ]
             ]
           ]
         ]
       ]
-    ]];
-*/  
+    ]
+  ]
+];
 echo json_encode($a);
 ?>
