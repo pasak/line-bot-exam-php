83d36426467fced25ff50889435c505f22f50cb8
pasak <pasaksoftware@gmail.com>
2018-06-29 16:24:03 +0700

    Update manager-push.php

--- manager-push.php
+++ manager-push.php
@@ -1,10 +1,29 @@
 <?php
 $_return = array();
 
-$FieldArray = array('access_token','LineUserID'); //,'messages');
+$FieldArray = array('access_token','PostURL');
 
 foreach ($FieldArray as $Field)
   $_return[$Field] = $_REQUEST[$Field];
 
+// $json = file_get_contents($url, false, stream_context_create($arrContextOptions));
+
 echo json_encode($_return,JSON_UNESCAPED_UNICODE);
+
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/push';
+
+$post = file_get_contents($PostURL);
+
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo "<br><br>result " $result . "\r\n";
 ?>
