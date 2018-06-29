a22fc5c62b6a35601f97efbe43f7804ba11ad82a
pasak <pasaksoftware@gmail.com>
2018-06-29 14:39:32 +0700

    Update manager-push.php

--- manager-push.php
+++ manager-push.php
@@ -1,18 +1,8 @@
 <?php
-$access_token = str_replace(' ','+',$_REQUEST['access_token']);
+$fp = fopen('php://input', 'r');
+$rawData = stream_get_contents($fp);
 
-$data = ['to' => $_REQUEST['LineUserID'],'messages' => $_REQUEST['messages']];
-
-$post = json_encode($data);
-
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+echo "<pre>";
+print_r($rawData);
+echo "</pre>";
 ?>
