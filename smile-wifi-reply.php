e7836272fa8a3d2e4ef44e228bbf9bad1e874fc7
pasak <pasaksoftware@gmail.com>
2019-07-05 13:26:26 +0700

    Update smile-wifi-reply.php

--- smile-wifi-reply.php
+++ smile-wifi-reply.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
       if ($event['source']['type'] == 'user') $ParentID = '' ;
       else $ParentID = ($event['source']['type'] == 'group') ? $event['source']['groupId'] : $event['source']['roomId'] ;
 
-      $url = 'https://smile.linebooking.co/liff/' . $_REQUEST['Group'] . '-reply.php?Token=' . $access_token .
+      $url = 'https://smilewifi.com/line/liff/' . $_REQUEST['Group'] . '-reply.php?Token=' . $access_token .
              '&SourceType=' . $event['source']['type'] . '&ParentID=' . $ParentID .
              '&UserID='. $event['source']['userId'] . '&ReplyToken=' . $event['replyToken'] ;
 
