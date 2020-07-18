ce97fde31c11632f81661a780c4036ae212cfcf6
pasak <pasaksoftware@gmail.com>
2020-07-18 15:36:13 +0700

    Update myq-reply.php

--- myq-reply.php
+++ myq-reply.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
       if ($event['source']['type'] == 'user') $ParentID = '' ;
       else $ParentID = ($event['source']['type'] == 'group') ? $event['source']['groupId'] : $event['source']['roomId'] ;
 
-      $url = 'https://my.lineservice.online/q/' . $_REQUEST['Group'] . '-reply.php?Token=' . $access_token .
+      $url = 'https://myq.link/q/' . $_REQUEST['Group'] . '-reply.php?Token=' . $access_token .
              '&SourceType=' . $event['source']['type'] . '&ParentID=' . $ParentID .
              '&UserID='. $event['source']['userId'] . '&ReplyToken=' . $event['replyToken'] ;
 
